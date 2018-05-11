3eed0f494c8bd897e5a3e49ae0c8584acdc89948
leadingtw <leadingtw@gmail.com>
2018-05-11 20:44:22 +0800

    把uk 拆分 並整理程式碼

--- chaosTest.js
+++ chaosTest.js
@@ -2,33 +2,49 @@ let chaos = require("./chaos_model.js");
 let f = require('float');
 
 
-let X = [0.7, -0.3, 1];
+let X = [0.5, -0.3, 0.4];
 let Y = [-0.3, -0.1, 0.8];
 
-const N = 50;
-const SYNC = 10;
+const N = 10000;
+const SYNC = 0;
 
 let tempX = [];
 let tempY = [];
 
-let u = 0;
+let Um = 0;
+let Us = 0;
+
+let count = 0;
 
 for (let i = 1; i <= N; i++) {
 
-    u = chaos.createUk(X, Y);
+    tempX = chaos.runHenonmap(i, X);
 
-    tempX[0] = chaos.X1(i, X);
-    tempX[1] = chaos.X2(i, X);
-    tempX[2] = chaos.X3(i, X);
+    tempY = chaos.runHenonmap(i, Y);
 
-    tempY[0] = (i < SYNC) ? chaos.X1(i, Y) : chaos.ukX1(i, Y, u);
-    tempY[1] = chaos.X2(i, Y);
-    tempY[2] = chaos.X3(i, Y);
+    if(i >= SYNC){
+        tempY[0] = f.round(tempY[0] + Um + Us, 10);
+    }
 
     X = tempX;
     Y = tempY;
 
-    console.log(`X1(${i}) = ${X[0]}\t\t,Y1(${i}) = ${Y[0]}\t\t, u => ${u}`);
+    Um = chaos.createUm(X);
+    Us = chaos.createUs(Y);
+
+    if (chaos.arraysEqual(X, Y)) {
+
+        if (count >= 10) {
+            break;
+        } else {
+            count++;
+        }
+
+    } else {
+        count = 0;
+    }
+
+    console.log(`X1(${i}) = ${X[0]}\t\t, Y1(${i}) = ${Y[0]}\t\t, ${count}`);
 }
 
 //chaos.henonMap(1000,y1,y2,y3);
--- chaos_model.js
+++ chaos_model.js
@@ -1,8 +1,4 @@
-let NP = require('number-precision');
-let big = require('big.js');
 let f = require('float');
-
-let e = [];
 
 const c1 = -1.7;
 const c2 = 0.72;
@@ -10,63 +6,39 @@ const c2 = 0.72;
 const A = -0.5;
 
 module.exports = {
-    ukX1: (k, x, u) => {
-        return (k <= 1) ? x[0] : f.round(1.76 - x[1] ** 2 - 0.1 * x[2] + u, 10);
-    },
-    X1: (k, x) => {
-        return (k <= 1) ? x[0] : f.round(1.76 - x[1] ** 2 - 0.1 * x[2], 10);
-    },
-    X2: (k, x) => {
-        return (k <= 1) ? x[1] : f.round(x[0], 10);
-    },
-    X3: (k, x) => {
-        return (k <= 1) ? x[2] : f.round(x[1], 10);
+    runHenonmap: (k, x) => {
+        let t = x.slice();
+        
+        if(k > 1){
+            t[0] = f.round(1.76 - x[1] ** 2 - 0.1 * x[2], 10);
+            t[1] = f.round(x[0], 10);
+            t[2] = f.round(x[1], 10);
+        }
+
+        return t;
     },
     createUk: (X, Y) => {
 
-        e[0] = f.round((Y[0] - X[0]), 10);
-        e[1] = f.round((Y[1] - X[1]), 10);
-        e[2] = f.round((Y[2] - X[2]), 10);
+        let Um = this.createUm(X);
+        let Us = this.createUs(Y);
 
-        //let s = f.round(e[0] + (c1 * e[1]) + (c2 * e[2]),10);
-        //let u = f.round(-((X[1]**2) - (Y[1]**2) - (0.1 * e[2]) + (c1 * e[0]) + (c2 * e[1]) - e[0] - (c1 * e[1]) - (c2 * e[2])) + (A * s),10);
-        let u = f.round((X[0] * (1 - c1 - A) + X[1] * (- X[1] - (A * c1) + c1 - c2) + X[2] * ((- A * c2) + c2 + 0.1)) + (Y[0] * (- 1 + A + c1) + Y[1] * (- Y[1] + (A * c1) + c2 - c1) + Y[2] * ((A * c2) - 0.1 - c2)), 10);
-
-        return f.round(u, 6);
+        return f.round(Um + Us, 10);
     },
     createUm: (X) => {
 
-        e[0] = f.round((Y[0] - X[0]), 10);
-        e[1] = f.round((Y[1] - X[1]), 10);
-        e[2] = f.round((Y[2] - X[2]), 10);
+        let Um = (-X[0] * (1 + A - c1) - X[1] * (X[1] + A * c1 - c2 + c1) - X[2] * (c2 + 0.1 + A * c2));
 
-        let s = f.round((Y[0] - X[0]) + (c1 * (Y[1] - X[1])) + (c2 * (Y[2] - X[2])), 10);
-        let u = f.round(-((X[1] ** 2) - (Y[1] ** 2) - (0.1 * (Y[2] - X[2])) + (c1 * (Y[0] - X[0])) + (c2 * (Y[1] - X[1])) - (Y[0] - X[0]) - (c1 * (Y[1] - X[1])) - (c2 * (Y[2] - X[2])) + (A * s), 10));
-
-
-        (X[0] * (1 - c1 - A) + X[1] * (- X[1] - (A * c1) + c1 - c2) + X[2] * (- A * c2 + c2 + 0.1))
-            // + X[0] * (1 - c1 - A)
-
-            // + X[1] * (- X[1] - (A * c1) + c1 - c2)
-
-            // + X[2] * (- A * c2 + c2 + 0.1)
-
-            + Y[0] * (- 1 + A + c1) + Y[1] * (- Y[1] + (A * c1) + c2 - c1) + Y[2] * (A * c2 - 0.1 - c2)
-        // + Y[0] * (- 1 + A + c1)
-
-        // + Y[1](- Y[1] + (A * c1) + c2 - c1)
-
-        // + Y[2] * (A * c2 - 0.1 - c2)
-
-        //+ (A * s)
-        //+ A * (Y[0] - X[0]) 
-
-
-
-        return f.round(u, 6);
+        return f.round(Um, 10);
 
     },
     createUs: (Y) => {
 
+        let Us = (Y[0] * (1 + A - c1) + Y[1] * (Y[1] + A * c1 - c2 + c1) + Y[2] * (c2 + 0.1 + A * c2));
+
+        return f.round(Us, 10);
+
+    },
+    arraysEqual: (a1,a2 ) => {
+        return JSON.stringify(a1)==JSON.stringify(a2);
     }
 }
