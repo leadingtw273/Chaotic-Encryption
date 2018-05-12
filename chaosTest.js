93e11e7117d3c9d9c4dab049de15ea578b68b22e
leadingtw <leadingtw@gmail.com>
2018-05-13 00:14:34 +0800

    升級 可調變

--- chaosTest.js
+++ chaosTest.js
@@ -1,6 +1,6 @@
 const chaos = require('./chaos_model.js');
 
-const N = 1000;
+const N = 10000;
 
 let X = [0.5, -0.3, 0.4];
 let Y = [-0.3, -0.1, 0.8];
@@ -17,6 +17,7 @@ for (let i = 1; i <= N; i++) {
   Y = chaos.runSlave(i, Y, Um);
 
   Um = chaos.createUm(X);
+  Us = chaos.createUs(Y);
 
   if (chaos.arraysEqual(X, Y)) {
 
@@ -30,5 +31,5 @@ for (let i = 1; i <= N; i++) {
     count = 0;
   }
 
-  console.log(`X1(${i}) = ${X[0].toFixed(6)}\t\t, Y1(${i}) = ${Y[0].toFixed(6)}\t\t, Uk = ${(Um + Us).toFixed(10)}\t\t, count = ${count}`);
+  console.log(`X1(${i}) = ${X[0].toFixed(10)}\t\t, Y1(${i}) = ${Y[0].toFixed(10)}\t\t, count = ${count}\t\t, Um = ${Um}\t\t, Us = ${Us}`);
 }
--- chaos_model.js
+++ chaos_model.js
@@ -1,18 +1,36 @@
 const f = require('float');
 
-const c1 = -1.7;
-const c2 = 0.72;
+let c = [-1.7, 0.72];
+let A = 0.9;
 
-const A = -0.5;
+let ax = [1,1,1];
+let dx = [1,1,1];
+
+let g = [];
+let h = [];
+let j = [];
+
+g[0] = -(ax[0] / (ax[1] * ax[1]));
+g[1] = 2 * ax[0] * dx[1] / (ax[1] * ax[1]);
+g[2] = -0.1 * ax[0] / ax[2];
+g[3] = ax[0] * (1.76 - (dx[1] * dx[1]) / (ax[1] * ax[1]) + 0.1 * ax[0] * dx[2] / ax[2]) + dx[0];
+
+h[0] = ax[1] / ax[0];
+h[1] = -(ax[1] * dx[0]) / ax[0] + dx[1];
+
+j[0] = ax[2] / ax[1];
+j[1] = -(ax[2] * dx[1]) / ax[1] + dx[2];
 
 const chaos = {
   runChaos: (k, x) => {
     let t = x.slice();
 
     if (k > 1) {
-      t[0] = f.round(1.76 - x[1] * x[1] - 0.1 * x[2], 10);
-      t[1] = f.round(x[0], 10);
-      t[2] = f.round(x[1], 10);
+
+      t[0] = f.round(g[0] * (x[1] * x[1]) + g[1] * x[1] + g[2] * x[2] + g[3], 10);
+      t[1] = f.round(h[0] * x[0] + h[1], 10);
+      t[2] = f.round(j[0] * x[1] + j[1], 10);
+
     }
 
     return t;
@@ -22,7 +40,7 @@ const chaos = {
   },
   runSlave: (k, x, Um) => {
     let t = chaos.runChaos(k, x);
-    if(k>1){
+    if (k > 1) {
       t[0] = f.round(t[0] + chaos.createUs(x) + Um, 10);
     }
     return t;
@@ -34,22 +52,27 @@ const chaos = {
 
     return f.round(Um + Us, 10);
   },
-  createUm: (X) => {
+  createUm: (x) => {
 
-    let Um = (-X[0] * (1 + A - c1) - X[1] * (X[1] + A * c1 - c2 + c1) - X[2] * (c2 + 0.1 + A * c2));
+    let Um = ((x[1] * x[1]) * g[0]) + (x[1] * g[1]) + (x[2] * g[2]) + (x[0] * c[0] * h[0]) + (x[1] * c[1] * j[0]) - (x[0] * 0.9) - (x[1] * c[0] * A) - (x[2] * c[1] * A);
 
     return f.round(Um, 10);
 
   },
-  createUs: (Y) => {
+  createUs: (y) => {
 
-    let Us = (Y[0] * (1 + A - c1) + Y[1] * (Y[1] + A * c1 - c2 + c1) + Y[2] * (c2 + 0.1 + A * c2));
+    let Us = (-(-y[1] * -y[1]) * g[0]) - (y[1] * g[1]) - (y[2] * g[2]) - (y[0] * c[0] * h[0]) - (y[1] * c[1] * j[0]) + (y[0] * 0.9) + (y[1] * c[0] * A) + (y[2] * c[1] * A);
 
     return f.round(Us, 10);
 
   },
   arraysEqual: (a1, a2) => {
-    return JSON.stringify(a1) == JSON.stringify(a2);
+
+    let p1 = a1[0].toFixed(6) == a2[0].toFixed(6);
+    let p2 = a1[1].toFixed(6) == a2[1].toFixed(6);
+    let p3 = a1[2].toFixed(6) == a2[2].toFixed(6);
+
+    return p1 && p2 && p3;
   }
 };
 
