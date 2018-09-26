8682a3b3e8a242635fa748cdff66089f93dcf0b2
leadingtw273 <leadingtw@gmail.com>
2018-09-26 16:21:18 +0800

    fix

--- imgEnc_all.js
+++ imgEnc_all.js
@@ -27,9 +27,9 @@ const AES_CBC = new AES256('aes-256-cbc', 'sha256', iv);
 const AES_ECB = new AES256('aes-256-ecb', 'sha256');
 
 // henon map 混沌系統模組實作
+const CHAOS_INIT_VALUE = [0.123456, 0.456789, 0.789123];
+const CHAOS_INIT_STEP = 1000;
 const chaos = new Chaos(0.001, [0.002, 0.003]);
-const chaosInitValue = [0.123456, 0.456789, 0.789123];
-const chaosInitStep = 1000;
 
 // 圖片檔名設置
 const imgNum = 50;
@@ -41,28 +41,29 @@ const inputPath = './cryptFile/org/';
 const outputPath = './cryptFile/enc/';
 
 // 混沌初始值跑 timeStep 次數 避免初始影響
-const chaosInit = (y, timeStep) => {
+const chaosInit = (initValue, timeStep) => {
   let step = 0;
+  let finValue = initValue;
   while (step < timeStep) {
-    y = chaos.runChaos(step, y);
+    finValue = chaos.runChaos(step, finValue);
     step++;
   }
-  return y;
+  return finValue;
 };
-let chaos_X = chaosInit(chaosInitValue, chaosInitStep);
 
-// AES金鑰設置
-const aesKey = chaosInit(chaosInitValue, chaosInitStep);
+// CHAOS 金鑰設置 使用[let]代表其值將會隨成是執行而改變
+let chaosKey = chaosInit(CHAOS_INIT_VALUE, CHAOS_INIT_STEP);
+// AES金鑰設置 使用[const]代表其值將無法做任何改變
+const aesKey = chaosInit(CHAOS_INIT_VALUE, CHAOS_INIT_STEP);
 
 // 主要加密程式執行
 for (let i = 0; i < imgNum; i++) {
   const inputFileName = fileName + i + fileExtension;
-  let step = chaosInitStep;
+  let chaosStep = CHAOS_INIT_STEP;
 
   jimp.read(inputPath + inputFileName, (err, img) => {
     if (err) throw err;
 
-    let tmp = iv;
     const orgBuf = inputImg(img);
 
     const aesBuf_ECB = dataCrypt(orgBuf, aesKey, AES_ECB);
@@ -70,30 +71,35 @@ for (let i = 0; i < imgNum; i++) {
 
     let chaosBuf_ECB = Buffer.alloc(0);
     let chaosBuf_CBC = Buffer.alloc(0);
+    let cbcXorData = iv;
 
     const readStream = streamifier.createReadStream(orgBuf);
 
     readStream.on('readable', () => {
       let chunk = '';
       while (null !== (chunk = readStream.read(16))) {
-        chaosBuf_ECB = Buffer.concat([
-          chaosBuf_ECB,
-          dataCrypt(chunk, chaos_X, AES_ECB)
-        ]);
+        // ECB 區塊加密
+        const chunk_ECB_data = dataCrypt(chunk, chaosKey, AES_ECB);
 
-        tmp = xor(tmp, chunk);
-        tmp = dataCrypt(tmp, chaos_X, AES_ECB);
-        dataCrypt(orgBuf, aesKey, AES_CBC);
-        chaosBuf_CBC = Buffer.concat([chaosBuf_CBC, tmp]);
+        // CBC 區塊加密
+        cbcXorData = xor(cbcXorData, chunk); // 執行XOR運算，此為CBC
+        cbcXorData = dataCrypt(cbcXorData, chaosKey, AES_ECB);
+        const chunk_CBC_data = cbcXorData;
 
-        step++;
-        chaos_X = chaos.runChaos(step, chaos_X);
+        // 將密文區塊串接
+        chaosBuf_ECB = Buffer.concat([chaosBuf_ECB, chunk_ECB_data]);
+        chaosBuf_CBC = Buffer.concat([chaosBuf_CBC, chunk_CBC_data]);
+
+        // 混沌金鑰運算
+        chaosStep++;
+        chaosKey = chaos.runChaos(chaosStep, chaosKey);
       }
     });
 
     readStream.on('end', () => {
       console.log(`${inputFileName} fin`);
 
+      // nist 測試文件輸出
       nistOutput({
         aesBuf_ECB,
         aesBuf_CBC,
@@ -101,8 +107,10 @@ for (let i = 0; i < imgNum; i++) {
         chaosBuf_CBC
       });
 
-      chaos_X = chaosInit(chaosInitValue, 1000);
+      // 重至金鑰
+      chaosKey = chaosInit(CHAOS_INIT_VALUE, 1000);
 
+      // 輸出圖像檔
       outputImg(aesBuf_ECB, img, 'aes_ECB', inputFileName);
       outputImg(aesBuf_CBC, img, 'aes_CBC', inputFileName);
       outputImg(chaosBuf_ECB, img, 'chaos_ECB', inputFileName);
